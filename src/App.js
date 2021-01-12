92954ce859dacdd3fefce809da34bcab681076fe
Kaeneth Dayao <kendayao@gmail.com>
2021-01-12 09:38:49 -0800

    setup webcam

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import WebcamCapture from './components/webcam-capture/WebcamCapture'
 
 function App() {
   return (
     <div className="app">
-     <h1>Snapchat</h1>
+     <WebcamCapture />
     </div>
   );
 }

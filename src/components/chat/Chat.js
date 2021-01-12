576148cc19f09c2974e73a2ac235e206d55ca766
Kaeneth Dayao <kendayao@gmail.com>
2021-01-12 13:15:48 -0800

    retreived post form db and display on front end

---
+++ src/components/chat/Chat.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Chat.css'
+import { Avatar } from '@material-ui/core'
+import StopRoundedIcon from '@material-ui/icons/StopRounded';
+import ReactTimeago from 'react-timeago';
+
+
+function Chat({id, username, timestamp, read, imageUrl, profilePic}) {
+    console.log(timestamp)
+    return (
+        <div className='chat'>
+            <Avatar className='chat__avatar' src={profilePic} />
+            <div className='chat__info'>
+                <h4>{username}</h4>
+                <p>Tap to view - <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()}/></p>
+            </div>
+            {!read && <StopRoundedIcon className='chat__readIcon' />}
+        </div>
+    )
+}
+
+export default Chat
+
--- src/components/chats/Chats.js
+++ src/components/chats/Chats.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import './Chats'
+import React, {useState, useEffect} from 'react'
+import './Chats.css'
+import { Avatar } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import Chat from '../chat/Chat'
+import { db } from '../../firebase'
 
 function Chats() {
+
+    const [posts, setPosts]=useState([]);
+
+    useEffect(()=>{
+        db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot=>
+            setPosts(snapshot.docs.map(doc=>({
+                id: doc.id,
+                data: doc.data()
+            })))
+        )
+    },[])
+
+    console.log(posts)
+
     return (
         <div className='chats'>
-            
+            <div className='chats__header'>
+                <Avatar className='chats__avatar'/>
+                <div className='chats__search'>
+                    <SearchIcon />
+                    <input placeholder="Friends" type="text"/>
+                </div>
+                <ChatBubbleIcon />
+            </div>
+            <div className='chats__posts'>
+                {posts.map(post=>(
+                    <Chat
+                        key={post.id}
+                        id={post.id}
+                        username={post.data.username}
+                        timestamp={post.data.timestamp}
+                        imageUrl={post.data.imageUrl}
+                        read={post.data.read}
+                        profilePic={post.data.profilePic}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
